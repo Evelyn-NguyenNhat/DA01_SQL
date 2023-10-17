76467c3a753ebea8ca03e9fdf5bd95902ccf4aec
Evelyn-NguyenNhat <147326749+Evelyn-NguyenNhat@users.noreply.github.com>
2023-10-17 12:26:20 +0700

    Update Practice4.sql

--- Practice4.sql
+++ Practice4.sql
@@ -10,3 +10,19 @@ CASE
     ELSE 'No' END AS triangle
 FROM Triangle
 /*ex3*/ 
+ SELECT
+  ROUND (100.0 * 
+    COUNT (CASE WHEN call_category IS NULL OR call_category = 'n/a' THEN case_id END) / COUNT (case_id), 1)
+AS uncategorised_call_pct
+FROM callers;
+/*Ex4*/
+SELECT  name
+FROM Customer
+WHERE referee_id <>2 OR referee_id IS NULL;
+/*ex5*/ 
+SELECT survived,
+   SUM(CASE WHEN pclass = 1 THEN 1 ELSE 0 END) AS first_class,
+   SUM(CASE WHEN pclass = 2 THEN 1 ELSE 0 END) AS second_class,
+   SUM(CASE WHEN pclass = 3 THEN 1 ELSE 0 END) AS third_class
+FROM titanic
+GROUP BY survived;
