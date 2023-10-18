53a70166b62142fd1bedbe9fb5160687bf4eb4ef
Evelyn-NguyenNhat <147326749+Evelyn-NguyenNhat@users.noreply.github.com>
2023-10-18 22:14:41 +0700

    Update PRACTICE5.sql

--- PRACTICE5.sql
+++ PRACTICE5.sql
@@ -4,6 +4,13 @@ FROM CITY
 INNER JOIN COUNTRY
 ON CITY.CountryCode = COUNTRY.code
 GROUP BY COUNTRY.CONTINENT;
+/*Ex2*/ 
+SELECT
+    ROUND(COUNT(texts.email_id) :: decimal / COUNT(DISTINCT emails.email_id), 2) AS activation_rate
+FROM emails
+left JOIN texts
+  ON emails.email_id = texts.email_id
+  AND texts.signup_action = 'Confirmed';
 /*ex3 */ 
 SELECT age.age_bucket,
   
@@ -16,6 +23,14 @@ FROM activities
 INNER JOIN age_breakdown AS age ON activities.user_id = age.user_id
 WHERE activities.activity_type IN ('open', 'send')
 GROUP BY age.age_bucket;
+/*Ex4*/
+SELECT customers.customer_id
+FROM customer_contracts AS customers
+LEFT JOIN products 
+ON customers.product_id=products.product_id
+where product_name like '%Azure%'
+GROUP BY customers.customer_id
+having COUNT (DISTINCT products.product_category) = 3
 /*ex5*/ 
 SELECT mng.employee_id, mng.name, COUNT(*) AS reports_count, 
 ROUND(AVG(emp.age)) AS average_age
