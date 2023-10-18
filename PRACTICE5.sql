3c3d81f64dd0b74f28ef4b9fbc795775dfd84a68
Evelyn-NguyenNhat <147326749+Evelyn-NguyenNhat@users.noreply.github.com>
2023-10-18 21:23:33 +0700

    Update PRACTICE5.sql

--- PRACTICE5.sql
+++ PRACTICE5.sql
@@ -4,7 +4,7 @@ FROM CITY
 INNER JOIN COUNTRY
 ON CITY.CountryCode = COUNTRY.code
 GROUP BY COUNTRY.CONTINENT;
-/*ex2 */ 
+/*ex3 */ 
 SELECT age.age_bucket,
   
   ROUND(100.0* SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'send')
@@ -16,3 +16,11 @@ FROM activities
 INNER JOIN age_breakdown AS age ON activities.user_id = age.user_id
 WHERE activities.activity_type IN ('open', 'send')
 GROUP BY age.age_bucket;
+/*ex5*/ 
+SELECT mng.employee_id, mng.name, COUNT(*) AS reports_count, 
+ROUND(AVG(emp.age)) AS average_age
+FROM employees AS emp
+INNER JOIN employees AS mng
+ON emp.reports_to= mng.employee_id 
+GROUP BY mng.employee_id, mng.name
+ORDER BY mng.employee_id 
