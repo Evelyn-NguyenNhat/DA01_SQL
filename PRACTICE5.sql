5d73972f082bf317acc838a2958f70d72546842a
Evelyn-NguyenNhat <147326749+Evelyn-NguyenNhat@users.noreply.github.com>
2023-10-18 20:20:21 +0700

    Update PRACTICE5.sql

--- PRACTICE5.sql
+++ PRACTICE5.sql
@@ -4,3 +4,15 @@ FROM CITY
 INNER JOIN COUNTRY
 ON CITY.CountryCode = COUNTRY.code
 GROUP BY COUNTRY.CONTINENT;
+/*ex2 */ 
+SELECT age.age_bucket,
+  
+  ROUND(100.0* SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'send')
+        / SUM(activities.time_spent), 2) AS send_percentage, 
+  ROUND(100.0* 
+  SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'open')
+        / SUM(activities.time_spent), 2) AS open_percentage
+FROM activities 
+INNER JOIN age_breakdown AS age ON activities.user_id = age.user_id
+WHERE activities.activity_type IN ('open', 'send')
+GROUP BY age.age_bucket;
