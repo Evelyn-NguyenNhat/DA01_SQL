e34dbe462af827f0d3250aa7929f3f5325b84b56
Evelyn-NguyenNhat <147326749+Evelyn-NguyenNhat@users.noreply.github.com>
2023-10-25 00:08:33 +0700

    Update Practice6.sql

--- Practice6.sql
+++ Practice6.sql
@@ -7,3 +7,16 @@ FROM job_listings
 GROUP BY company_id, title, description
 HAVING COUNT(job_id)>1
 ) AS sunquery;
+/** EX2**/ 
+SELECT category, product, total_spend
+FROM 
+(SELECT category, product, 
+SUM(spend) AS total_spend, 
+RANK()OVER (
+    PARTITION BY category
+    ORDER BY SUM(spend) DESC) AS ranking 
+    FROM product_spend 
+    WHERE EXTRACT(YEAR FROM transaction_date)=2022
+    GROUP BY category, product ) AS subquery
+WHERE ranking<=2
+ORDER BY category, ranking;
