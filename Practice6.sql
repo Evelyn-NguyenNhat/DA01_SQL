d2cb47e30865e8fdbb4881f2299e316a4059325c
Evelyn-NguyenNhat <147326749+Evelyn-NguyenNhat@users.noreply.github.com>
2023-10-26 00:25:29 +0700

    Update Practice6.sql

--- Practice6.sql
+++ Practice6.sql
@@ -106,3 +106,16 @@ UNION
     ORDER BY AVG(rating) DESC, 1 ASC
     LIMIT 1
 )
+/** Ex12**/ 
+SELECT id, SUM(num) AS num
+FROM (
+  SELECT accepter_id AS id, COUNT(requester_id) AS num 
+  FROM RequestAccepted
+  GROUP BY accepter_id
+  UNION ALL
+  SELECT requester_id AS id, COUNT(accepter_id) AS num 
+  FROM RequestAccepted
+  GROUP BY requester_id) AS subquery
+GROUP BY id
+ORDER BY SUM(num) DESC 
+LIMIT 1
