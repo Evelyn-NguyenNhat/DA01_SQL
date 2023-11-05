dbe5b7d037ac1022766c7b7cd820369efc674209
Evelyn-NguyenNhat <147326749+Evelyn-NguyenNhat@users.noreply.github.com>
2023-11-06 00:51:55 +0700

    Create Practice8.sql

---
+++ Practice8.sql
@@ -0,0 +1,11 @@
+/* Ex1 */ 
+SELECT 
+(ROUND (SUM(CASE WHEN order_date=customer_pref_delivery_date THEN 1 ELSE 0 END)
+/ COUNT(*), 2)*100) AS immediate_percentage 
+FROM delivery 
+WHERE (customer_id,order_date) IN 
+(
+SELECT customer_id, min(order_date)
+FROM delivery 
+GROUP BY customer_id);
+/* Ex2*/
